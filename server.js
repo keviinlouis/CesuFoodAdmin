23411df979f5275815b73b7a1514321c1825fad8
Kevin  Louis <keviinlouiis@gmail.com>
2018-05-31 02:38:31 -0300

    server

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+/**************************************************************************
+ IMPORTS
+ **************************************************************************/
+
+
 var express = require('express')
+var history = require('connect-history-api-fallback')
 var path = require('path')
+var serveStatic = require('serve-static')
+
+
+/**************************************************************************
+ EXPRESS SERVER
+ **************************************************************************/
+
 
 var app = express()
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist'))
-})
+
+app.use(history({verbose: true}))
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
 var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+
+app.listen(port, () => {
+  console.log('Server started at http://localhost:5000')
+})
